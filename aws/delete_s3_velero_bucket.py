de05a36842a993758844a354f3623b1067dc3c14
Israel Pinto <ipinto@redhat.com>
2023-03-14 17:15:50 +0200

    using click for args input

--- aws/delete_s3_velero_bucket.py
+++ aws/delete_s3_velero_bucket.py
@@ -3,9 +3,9 @@ The main purpose of this script is to delete a bucket associated with a cluster
 after the cluster is uninstalled:
 -   The script will delete the velero bucket that is mapped to a cluster.
 
-Required (environment Variable):
-    AWS_ACCESS_KEY_ID
-    AWS_SECRET_ACCESS_KEY
+Required:
+    AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY as environment variable or input argument
+    cluster name as argument
 
 Usage:
 1. The module can be imported and used like below :
@@ -15,14 +15,10 @@ Usage:
     delete_s3_bucket.delete_velero_cluster_buckets(cluster)
     -   cluster: the name of the cluster
 
-2.  python delete_s3_velero_bucket.py -c|--cluster <cluster_name>
-
-    example:
-    1. python delete_s3_velero_bucket.py -c mts-rhods-c1
-    2. python delete_s3_velero_bucket.py --cluster mts-rhoam-c2
+2.  python delete_s3_velero_bucket.py
+        --aws_access_key_id=<access-key> --aws_secret_access_key=<secret> -c|--cluster <cluster_name>
 
 """
-import argparse
 import json
 import logging
 import os
@@ -32,53 +28,56 @@ from http import HTTPStatus
 from typing import Union
 
 import boto3
+import click
 
 LOGGER = logging.getLogger(__name__)
-S3_CLIENT = boto3.client("s3")
-REQUIRE_ENV = ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"]
 
 
-def delete_velero_cluster_buckets(cluster) -> None:
+def delete_velero_cluster_buckets(cluster, boto_client) -> None:
     """
     Delete the velero bucket associated with a cluster
 
     Args:
         cluster: The cluster name
+        boto_client: aws boto client
+
 
     """
 
     LOGGER.info(f"Delete velero buckets for '{cluster}' cluster")
 
-    velero_bucket_list = get_velero_buckets()
+    velero_bucket_list = get_velero_buckets(boto_client=boto_client)
     if not velero_bucket_list:
         LOGGER.info("No velero buckets found")
         return
 
     for bucket in velero_bucket_list:
+        bucket_name = bucket["Name"]
         if verify_cluster_matches_velero_infrastructure_name(
+            boto_client=boto_client,
             cluster_name=cluster,
-            bucket_name=bucket["Name"],
+            bucket_name=bucket_name,
         ):
             delete_all_objects_from_s3_folder(
-                bucket_name=bucket["Name"],
+                bucket_name=bucket_name, boto_client=boto_client
             )
-
-            delete_bucket(bucket_name=bucket["Name"])
+            delete_bucket(bucket_name=bucket_name, boto_client=boto_client)
             # A cluster is mapped to only one bucket, so don't check any remaining buckets
             return
 
     LOGGER.info("No buckets deleted")
 
 
-def delete_bucket(bucket_name: str) -> None:
+def delete_bucket(bucket_name: str, boto_client) -> None:
     """
     Delete velero bucket
 
     Args:
         bucket_name: the bucket name
+        boto_client: AWS client
     """
-    LOGGER.info(f"Delete cluster bucket {bucket_name}")
-    response = S3_CLIENT.delete_bucket(Bucket=bucket_name)
+    LOGGER.info(f"Delete bucket {bucket_name}")
+    response = boto_client.delete_bucket(Bucket=bucket_name)
 
     response_http_status_code = response["ResponseMetadata"]["HTTPStatusCode"]
     if response_http_status_code == HTTPStatus.NO_CONTENT:
@@ -86,7 +85,7 @@ def delete_bucket(bucket_name: str) -> None:
         return
 
     elif response_http_status_code == HTTPStatus.NOT_FOUND:
-        LOGGER.info(f"Bucket '{bucket_name}' not found\n")
+        LOGGER.info(f"Bucket '{bucket_name}' not found")
         return
 
     LOGGER.error(
@@ -96,16 +95,19 @@ def delete_bucket(bucket_name: str) -> None:
     sys.exit(1)
 
 
-def get_velero_buckets() -> list:
+def get_velero_buckets(boto_client) -> list:
     """
     Get a list of velero buckets
+
+    Args:
+        boto_client: AWS client
 
     Returns:
         list of velero buckets
     """
     LOGGER.info("Get a list of velero buckets")
 
-    buckets = S3_CLIENT.list_buckets()["Buckets"]
+    buckets = boto_client.list_buckets()["Buckets"]
     return [
         bucket
         for bucket in buckets
@@ -113,19 +115,20 @@ def get_velero_buckets() -> list:
     ]
 
 
-def get_velero_infrastructure_name(bucket_name: str) -> Union[str, None]:
+def get_velero_infrastructure_name(bucket_name: str, boto_client) -> Union[str, None]:
     """
     Get the velero bucket infrastructure name
 
     Args:
         bucket_name: The name of the cluster bucket to delete
+        boto_client: AWS client
 
     Returns:
-        velero infrastructure name or None
+        str or None: velero infrastructure name if found else None
     """
 
     LOGGER.info(f"Get tags for bucket: {bucket_name}")
-    bucket_tags = S3_CLIENT.get_bucket_tagging(Bucket=bucket_name)["TagSet"]
+    bucket_tags = boto_client.get_bucket_tagging(Bucket=bucket_name)["TagSet"]
 
     for tag in bucket_tags:
         if tag["Key"] == "velero.io/infrastructureName":
@@ -133,6 +136,7 @@ def get_velero_infrastructure_name(bucket_name: str) -> Union[str, None]:
 
 
 def verify_cluster_matches_velero_infrastructure_name(
+    boto_client,
     cluster_name: str,
     bucket_name: str,
 ) -> bool:
@@ -140,18 +144,21 @@ def verify_cluster_matches_velero_infrastructure_name(
     Get the velero bucket infrastructure name and compare it with the cluster
 
     Args:
+        boto_client: AWS client
         cluster_name: The cluster name
         bucket_name: The bucket name
 
     Returns:
-        True if the cluster matches a velero infrastructure name, False otherwise
+         bool: True if the cluster matches a velero infrastructure name, False otherwise
     """
 
     LOGGER.info(
         f"Verify cluster matches a velero infrastructure name via its bucket tag: {bucket_name}",
     )
 
-    velero_infrastructure_name = get_velero_infrastructure_name(bucket_name=bucket_name)
+    velero_infrastructure_name = get_velero_infrastructure_name(
+        bucket_name=bucket_name, boto_client=boto_client
+    )
 
     # Verify if the bucket is associated with the cluster
     if velero_infrastructure_name and re.search(
@@ -167,15 +174,16 @@ def verify_cluster_matches_velero_infrastructure_name(
     return False
 
 
-def delete_all_objects_from_s3_folder(bucket_name: str) -> None:
+def delete_all_objects_from_s3_folder(bucket_name: str, boto_client) -> None:
     """
     Deletes all files in a folder of an S3 bucket
 
     Args:
         bucket_name: The bucket name
+        boto_client: AWS client
     """
 
-    list_obj_response = S3_CLIENT.list_objects_v2(Bucket=bucket_name)
+    list_obj_response = boto_client.list_objects_v2(Bucket=bucket_name)
 
     #  Sometimes there maybe no contents -- no objects
     files_in_folder = list_obj_response.get("Contents")
@@ -184,7 +192,7 @@ def delete_all_objects_from_s3_folder(bucket_name: str) -> None:
         return
 
     files_to_delete = [{"Key": file["Key"]} for file in files_in_folder]
-    delete_response = S3_CLIENT.delete_objects(
+    delete_response = boto_client.delete_objects(
         Bucket=bucket_name,
         Delete={"Objects": files_to_delete, "Quiet": True},
     )
@@ -203,31 +211,34 @@ def delete_all_objects_from_s3_folder(bucket_name: str) -> None:
         sys.exit(1)
 
 
-def main():
-    found_env = []
-    for env_var in REQUIRE_ENV:
-        if os.getenv(env_var):
-            found_env.append(env_var)
-    if len(found_env) < len(REQUIRE_ENV):
-        LOGGER.error(
-            f"Missing environment variable found: {found_env}, require:{REQUIRE_ENV}"
-        )
-        sys.exit(1)
-
-    parser = argparse.ArgumentParser(description="Delete an s3 velero cluster bucket")
-    parser.add_argument(
-        "-c",
-        "--cluster_name",
-        help=""" Either set the S3_CLUSTER environment variable or
-                    Pass the cluster value at the command line when the script is invoked with the -c|--cluster
-                """,
+@click.command()
+@click.option(
+    "--aws-access-key-id",
+    help="Set AWS access key id, default if taken from environment variable: AWS_ACCESS_KEY_ID",
+    required=True,
+    default=os.getenv("AWS_ACCESS_KEY_ID"),
+)
+@click.option(
+    "--aws-secret-access-key",
+    help="Set AWS secret access key id,default if taken from environment variable: AWS_SECRET_ACCESS_KEY",
+    required=True,
+    default=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
+@click.option(
+    "-c",
+    "--cluster-name",
+    help="",
+    required=True,
+    default=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
+def main(aws_access_key_id, aws_secret_access_key, cluster_name):
+    boto_client = boto3.client(
+        service_name="s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
     )
-    cluster_name = parser.parse_args().cluster_name
-    if not cluster_name:
-        LOGGER.error("Missing cluster name")
-        sys.exit(1)
 
-    delete_velero_cluster_buckets(cluster=cluster_name)
+    delete_velero_cluster_buckets(cluster=cluster_name, boto_client=boto_client)
 
 
 if __name__ == "__main__":
